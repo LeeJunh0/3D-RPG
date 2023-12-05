1cecd4fca5b412d1630f6e56e158af23b1f9dfe8
LeeJunho <tjdi146534@gmail.com>
2023-12-05 17:46:02 +0900

    공격 콤보시스템 구축중

--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    enum PlayerState { IDLE, MOVE, ATTACK };
+
     [SerializeField]
     private Transform Camera;
     [SerializeField]
@@ -14,10 +16,15 @@ public class Controller : MonoBehaviour
     private float RunSpeed;
     [SerializeField]
     private GameObject PlayerObject;
-
+    [SerializeField]
+    private PlayerState CurState = PlayerState.IDLE;
+    [SerializeField]
+    private float ComboDeley;
     Rigidbody Rigid;
     Animator anim;
-    
+    Vector2 MoveInput;
+    int AttNum;
+    bool UnderAttack = false;
     void Start()
     {
         Rigid = GetComponent<Rigidbody>();
@@ -26,33 +33,92 @@ public class Controller : MonoBehaviour
     
     private void FixedUpdate()
     {
-        Moving();
+        MoveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isCheck = MoveInput.magnitude != 0;
+        switch (CurState)
+        {
+            case PlayerState.IDLE:
+                anim.SetFloat("Move", 0f);
+
+                if (isCheck == true)
+                {
+                    CurState = PlayerState.MOVE;
+                }
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+                    CurState = PlayerState.ATTACK;
+                }
+                break;
+            case PlayerState.MOVE:
+                Moving();
+
+                if(isCheck == false)
+                {
+                    CurState = PlayerState.IDLE;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    CurState = PlayerState.ATTACK;
+                }
+                break;
+            case PlayerState.ATTACK:
+                if(UnderAttack != true)
+                {
+                    Attack(AttNum);
+                }
+
+                if(isCheck == true)
+                {
+                    CurState = PlayerState.MOVE;
+                }
+                else
+                {
+                    CurState = PlayerState.IDLE;
+                }
+                break;
+        }     
+        
         CameraMove();
     }
 
-    void Moving()
+    private void Moving()
     {
         Debug.DrawRay(Camera.position, new Vector3(Camera.forward.x, 0f, Camera.forward.z), Color.blue);
-        Vector2 MoveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        bool isCheck = MoveInput.magnitude != 0;
-        if(isCheck == true)
-        {
-            anim.SetFloat("Move", 0.2f);
+        anim.SetFloat("Move", 0.2f);
 
-            Vector3 LookForward = new Vector3(Camera.forward.x, 0f, Camera.forward.z).normalized;
-            Vector3 LookRight = new Vector3(Camera.right.x, 0f, Camera.right.z).normalized;
-            Vector3 MoveDir = LookForward * MoveInput.y + LookRight * MoveInput.x;
+        Vector3 LookForward = new Vector3(Camera.forward.x, 0f, Camera.forward.z).normalized;
+        Vector3 LookRight = new Vector3(Camera.right.x, 0f, Camera.right.z).normalized;
+        Vector3 MoveDir = LookForward * MoveInput.y + LookRight * MoveInput.x;
 
-            PlayerObject.transform.forward = MoveDir;
-            Rigid.velocity = MoveDir * Time.deltaTime * MoveCheck(Input.GetKey(KeyCode.LeftShift));
-        }
-        else
-        {
-            anim.SetFloat("Move", 0f);
-        }
+        PlayerObject.transform.forward = MoveDir;
+        Rigid.velocity = MoveDir * Time.deltaTime * MoveCheck(Input.GetKey(KeyCode.LeftShift));
     }
 
+    private void Attack(int num)
+    {
+        anim.SetFloat("Combo", num);
+        anim.SetTrigger("Attack");
+        StartCoroutine(Combo());
+    }
+
+    IEnumerator Combo()
+    {
+        float curtime = 0f;
+        yield return null;
+
+        while(curtime >= ComboDeley || AttNum < 3)
+        {
+            curtime += Time.deltaTime;
+            if(Input.GetMouseButtonDown(0))
+            {
+                Attack(AttNum++);
+            }
+        }
+        AttNum = 0;
+    }
     private float MoveCheck(bool Check)
     {
         anim.SetBool("RunCheck", Check);
